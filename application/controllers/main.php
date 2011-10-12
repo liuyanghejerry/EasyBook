f72d1ac795d329f9ffc3f2598ab135c44670ddaa
liuyanghejerry <liuyanghejerry@126.com>
2011-10-12 17:07:03 +0800

    Init.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

---
+++ application/controllers/main.php
@@ -0,0 +1,32 @@
+<?php
+class Main extends CI_Controller {
+
+ function __construct()
+ {
+  parent::__construct();
+  $this->load->helper('html');
+ }
+ 
+ function _makeHeader($data)
+ {
+  echo doctype('html5');
+$data['keywords'] = 'This, is, keywords';
+$data['description'] = 'This is description';
+$data['robots'] = 'This is robots part';
+return $data;
+ }
+ 
+ function index()
+ {
+ $data = array(
+'hello' => 'My Title'
+);
+  $data = $this->_makeHeader($data);
+  $this->load->view('header',$data);
+  $this->load->view('main');
+  
+  //echo 'Hello, world';
+ }
+
+}
+?>
---
+++ application/views/header.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<meta http-equiv="content-Type" content="text/html; charset=utf8"/>
+<meta name ="keywords" content="<?=$keywords?>">
+<meta name="description" content="<?=$description?>">
+<meta name="robots" content="<?=$robots?>">
+</head>
+<body>
+<div id="header">
+
+</div>
