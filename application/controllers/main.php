524d8c86cc0772922c0ce2ed16efae7dd274891c
liuyanghejerry <liuyanghejerry@126.com>
2011-10-14 23:09:06 +0800

    new header template.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,32 +1,35 @@
 <?php
-class Main extends CI_Controller {
-
- function __construct()
- {
-  parent::__construct();
-  $this->load->helper('html');
- }
- 
- function _makeHeader($data)
- {
-  echo doctype('html5');
-$data['keywords'] = 'This, is, keywords';
-$data['description'] = 'This is description';
-$data['robots'] = 'This is robots part';
-return $data;
- }
- 
- function index()
- {
- $data = array(
-'hello' => 'My Title'
-);
-  $data = $this->_makeHeader($data);
-  $this->load->view('header',$data);
-  $this->load->view('main');
-  
-  //echo 'Hello, world';
- }
-
-}
+class Main extends CI_Controller{
+    
+     function __construct()
+    {
+         parent :: __construct();
+         $this -> load -> helper('html');
+		 $this->load->library('javascript');
+		 $this->load->library('jquery');
+         }
+    
+     function _makeHeader($data)
+    {
+         echo doctype('html5');
+        $data['keywords'] = 'This, is, keywords';
+        $data['description'] = 'This is description';
+        $data['robots'] = 'This is robots part';
+        return $data;
+         }
+    
+     function index()
+    {
+         $data = array(
+            'hello' => 'My Title'
+            );
+         $data = $this -> _makeHeader($data);
+         $this -> load -> view('header', $data);
+		 //$this -> load -> view('resource/header.css', $data);
+         $this -> load -> view('main');
+        
+         // echo 'Hello, world';
+    }
+    
+    }
 ?>
--- application/views/header.php
+++ application/views/header.php
@@ -1,11 +1,41 @@
 <html>
 <head>
-<meta http-equiv="content-Type" content="text/html; charset=utf8"/>
+<meta http-equiv="content-Type" content="text/html" charset="utf-8">
 <meta name ="keywords" content="<?=$keywords?>">
 <meta name="description" content="<?=$description?>">
 <meta name="robots" content="<?=$robots?>">
+<link rel="stylesheet" type="text/css" media="all" href="resource/header.css">
 </head>
 <body>
-<div id="header">
+
+<div class="page-top-holder">
+
+<div class="page-head-holder">
+<div class="search">
+<form action="#" method="get" id="search-sug">
+<fieldset>
+<div class="search-input">
+<input accesskey="s" size="24" class="sug-input" autocomplete="off" name="key" value="搜索框预置内容" id="search-sug-input">
+</div>
+<button type="submit"></button>
+<div id="loading-pic"></div>
+<div class="sug-result">
+</div>		
+</fieldset>
+</form>
+</div>
+<ul class="main-nav nav-plaza">
+<li><a href="#">集市</a></li>
+<li><a href="#">排行榜</a></li>
+<li><a href="#">留言板</a></li>
+<li><a href="#">我的账户</a></li>
+</ul>
+<div class="user-bar">
+<a href="#" id="login-hook">登录</a>&nbsp;&nbsp;<a href="#">注册</a> 
 
 </div>
+<div class="logo"> 
+<a href="#"><img src="resource/logo.png" width="159" height="67"></a> 
+</div>
+</div>
+</div> 
