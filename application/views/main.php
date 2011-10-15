f68a5e8f9944a5ffc65a3da350679781020028a3
liuyanghejerry <liuyanghejerry@126.com>
2011-10-15 18:32:52 +0800

    move the background pic and set new layout for the 2nd-floor.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- application/views/main.php
+++ application/views/main.php
@@ -1,6 +1,6 @@
 <div id="main-box">
 <div class="white-box">
-<a class ="more" href="#">更多...</a>
+<a class ="more" href="#">更多-></a>
 <h2>最新售出</h2>
 <div id="ps_slider" class="ps_slider">
 			<a class="prev disabled"></a>
@@ -16,7 +16,7 @@
 				<div class="ps_album" style="opacity:0;"><img src="resource/main/8.jpg" alt=""/><div class="ps_desc"><h2>Heartbreaks</h2><span>A light moving. Moved was abundantly created, over sixth above grass after grass.</span></div></div>
 				<div class="ps_album" style="opacity:0;"><img src="resource/main/9.jpg" alt=""/><div class="ps_desc"><h2>Fearful Water</h2><span>If the doors of perception were cleansed everything would appear to man as it is, infinite.</span></div></div>
 			</div>	
-		</div>
+</div>
 		
 		<div id="ps_overlay" class="ps_overlay" style="display:none;"></div>
 		<a id="ps_close" class="ps_close" style="display:none;"></a>
@@ -24,7 +24,184 @@
 			<a id="ps_next_photo" class="ps_next_photo" style="display:none;"></a>
 		</div>
 </div>
+<div id="center-two">
+<div class="white-box" id="left-box">
+<h2>求购榜</h2>
+<ul class="hot-list">
+<li>
+<h3><a href="#">《经济法》</a></h3>
+<div>
+<span class="li-left">出版社</span><span class="li-right">人民教育出版社</span>
+</div>
+<div>
+<span class="li-left">ISBN</span><span class="li-right">123-321-23123123</span>
+</div>
+</li>
+<li>
+<h3><a href="#">《经济法》</a></h3>
+<div>
+<span class="li-left">出版社</span><span class="li-right">人民教育出版社</span>
+</div>
+<div>
+<span class="li-left">ISBN</span><span class="li-right">123-321-23123123</span>
+</div>
+</li>
+<li>
+<h3><a href="#">《经济法》</a></h3>
+<div>
+<span class="li-left">出版社</span><span class="li-right">人民教育出版社</span>
+</div>
+<div>
+<span class="li-left">ISBN</span><span class="li-right">123-321-23123123</span>
+</div>
+</li>
+<li>
+<h3><a href="#">《经济法》</a></h3>
+<div>
+<span class="li-left">出版社</span><span class="li-right">人民教育出版社</span>
+</div>
+<div>
+<span class="li-left">ISBN</span><span class="li-right">123-321-23123123</span>
+</div>
+</li>
+<li>
+<h3><a href="#">《经济法》</a></h3>
+<div>
+<span class="li-left">出版社</span><span class="li-right">人民教育出版社</span>
+</div>
+<div>
+<span class="li-left">ISBN</span><span class="li-right">123-321-23123123</span>
+</div>
+</li>
+<li>
+<h3><a href="#">《经济法》</a></h3>
+<div>
+<span class="li-left">出版社</span><span class="li-right">人民教育出版社</span>
+</div>
+<div>
+<span class="li-left">ISBN</span><span class="li-right">123-321-23123123</span>
+</div>
+</li>
+</ul>
+<a href="#" class="more">完整榜单-></a>
+</div>
+<div class="white-box" id="right-box">
+<h2>火热推荐</h2>
+<div class="cover_box">
+    <div class="cover_item_box">
+        <div class="cover_item_imgbox"><a href="#" target="_blank">
+            <img id="CoverList2_RepeaterUp_ctl00_Image1" src="resource/main/1.jpg" style="border-width:0px;">
+        </a></div>
+        <div class="cover_item_name">
+            <a href="/BookBook/ShowBook.aspx?BookID=73" target="_blank">计算机算法设计与分析（第三版）</a>
+        </div>
+        <div class="cover_item_price">
+            <i><span id="CoverList2_RepeaterUp_ctl00_lblrec">推荐31次</span></i>
+            <span id="CoverList2_RepeaterUp_ctl00_lblprice">现价:￥10</span>
+        </div>
+    </div>
+    
 
+    <div class="cover_item_box">
+        <div class="cover_item_imgbox"><a href="#" target="_blank">
+            <img id="CoverList2_RepeaterUp_ctl01_Image1" src="resource/main/2.jpg" style="border-width:0px;">
+        </a></div>
+        <div class="cover_item_name">
+            <a href="/BookBook/ShowBook.aspx?BookID=69" target="_blank">模拟CMOS集成电路设计</a>
+        </div>
+        <div class="cover_item_price">
+            <i><span id="CoverList2_RepeaterUp_ctl01_lblrec">推荐21次</span></i>
+            <span id="CoverList2_RepeaterUp_ctl01_lblprice">现价:￥30</span>
+        </div>
+    </div>
+    
+
+    <div class="cover_item_box">
+        <div class="cover_item_imgbox"><a href="#" target="_blank">
+            <img id="CoverList2_RepeaterUp_ctl02_Image1" src="resource/main/3.jpg" style="border-width:0px;">
+        </a></div>
+        <div class="cover_item_name">
+            <a href="/BookBook/ShowBook.aspx?BookID=133" target="_blank">当代世界经济与政治</a>
+        </div>
+        <div class="cover_item_price">
+            <i><span id="CoverList2_RepeaterUp_ctl02_lblrec">推荐14次</span></i>
+            <span id="CoverList2_RepeaterUp_ctl02_lblprice">现价:￥5</span>
+        </div>
+    </div>
+	
+	
+	<div class="cover_item_box">
+        <div class="cover_item_imgbox"><a href="#" target="_blank">
+            <img id="CoverList2_RepeaterUp_ctl02_Image1" src="resource/main/4.jpg" style="border-width:0px;">
+        </a></div>
+        <div class="cover_item_name">
+            <a href="/BookBook/ShowBook.aspx?BookID=133" target="_blank">当代世界经济与政治</a>
+        </div>
+        <div class="cover_item_price">
+            <i><span id="CoverList2_RepeaterUp_ctl02_lblrec">推荐14次</span></i>
+            <span id="CoverList2_RepeaterUp_ctl02_lblprice">现价:￥5</span>
+        </div>
+    </div>
+</div>
+<div class="cover_box">
+    <div class="cover_item_box">
+        <div class="cover_item_imgbox"><a href="#" target="_blank">
+            <img id="CoverList2_RepeaterUp_ctl00_Image1" src="resource/main/5.jpg" style="border-width:0px;">
+        </a></div>
+        <div class="cover_item_name">
+            <a href="/BookBook/ShowBook.aspx?BookID=73" target="_blank">计算机算法设计与分析（第三版）</a>
+        </div>
+        <div class="cover_item_price">
+            <i><span id="CoverList2_RepeaterUp_ctl00_lblrec">推荐31次</span></i>
+            <span id="CoverList2_RepeaterUp_ctl00_lblprice">现价:￥10</span>
+        </div>
+    </div>
+    
+
+    <div class="cover_item_box">
+        <div class="cover_item_imgbox"><a href="#" target="_blank">
+            <img id="CoverList2_RepeaterUp_ctl01_Image1" src="resource/main/6.jpg" style="border-width:0px;">
+        </a></div>
+        <div class="cover_item_name">
+            <a href="/BookBook/ShowBook.aspx?BookID=69" target="_blank">模拟CMOS集成电路设计</a>
+        </div>
+        <div class="cover_item_price">
+            <i><span id="CoverList2_RepeaterUp_ctl01_lblrec">推荐21次</span></i>
+            <span id="CoverList2_RepeaterUp_ctl01_lblprice">现价:￥30</span>
+        </div>
+    </div>
+    
+
+    <div class="cover_item_box">
+        <div class="cover_item_imgbox"><a href="#" target="_blank">
+            <img id="CoverList2_RepeaterUp_ctl02_Image1" src="resource/main/7.jpg" style="border-width:0px;">
+        </a></div>
+        <div class="cover_item_name">
+            <a href="/BookBook/ShowBook.aspx?BookID=133" target="_blank">当代世界经济与政治</a>
+        </div>
+        <div class="cover_item_price">
+            <i><span id="CoverList2_RepeaterUp_ctl02_lblrec">推荐14次</span></i>
+            <span id="CoverList2_RepeaterUp_ctl02_lblprice">现价:￥5</span>
+        </div>
+    </div>
+	
+	
+	<div class="cover_item_box">
+        <div class="cover_item_imgbox"><a href="#" target="_blank">
+            <img id="CoverList2_RepeaterUp_ctl02_Image1" src="resource/main/8.jpg" style="border-width:0px;">
+        </a></div>
+        <div class="cover_item_name">
+            <a href="/BookBook/ShowBook.aspx?BookID=133" target="_blank">当代世界经济与政治</a>
+        </div>
+        <div class="cover_item_price">
+            <i><span id="CoverList2_RepeaterUp_ctl02_lblrec">推荐14次</span></i>
+            <span id="CoverList2_RepeaterUp_ctl02_lblprice">现价:￥5</span>
+        </div>
+    </div>
+</div>
+<a href="#" class="more">全部推荐-></a>
+</div>
+</div>
 </div>
 
 </body>
