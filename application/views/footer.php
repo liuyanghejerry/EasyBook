2830bd9c0566687fcab17e7ddd546ea104e821dc
liuyanghejerry <liuyanghejerry@126.com>
2011-10-15 20:00:29 +0800

    add footer.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

---
+++ application/views/footer.php
@@ -0,0 +1,39 @@
+<div id="footer">
+<div class="footer_box">
+            <div class="footer_item">
+                <h2 class="footer_item_title">关于我们</h2>
+                <ul>
+                    <li><a href="#">如何使用</a></li>
+                    <li><a href="#">留言与建议</a></li>
+                    <li><a href="#">加入易书</a></li>
+                    <li><a href="#">里程碑</a></li>
+                </ul>
+            </div>
+            <div class="footer_item">
+                <h2 class="footer_item_title">友情链接</h2>
+                    <ul>
+                        <li><a href="#">江南大学</a></li>
+                        <li><a href="#">江南学子</a></li>
+                    </ul>
+            </div>
+            <div class="footer_item">
+                <h2 class="footer_item_title">我的账户</h2>
+                <ul>
+                    <li><a href="#">用户中心</a></li>
+                    <li><a href="#">我的出售</a></li>
+                    <li><a href="#">我的求购</a></li>
+                </ul>
+            </div>
+            <div class="footer_item">
+                <h2 class="footer_item_title">快捷方式</h2>
+                <ul>
+                    <li><a href="#">所有出售</a></li>
+                    <li><a href="#">所有求购</a></li>
+                    <li><a href="#">管理入口</a></li>
+                </ul>
+            </div>
+     </div>
+	 <div id="copyright"><span>Copyright 易书网 2011 , All Rights Reserved.</span></div>
+</div>
+</body>
+</html>
