8dbe04b2f17ac1fc9447458fb8c6ec7c573e5e94
liuyanghejerry <liuyanghejerry@126.com>
2011-10-17 20:28:19 +0800

    add 404 page.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

---
+++ application/controllers/missing.php
@@ -0,0 +1,35 @@
+<?php
+class Missing extends CI_Controller{
+    
+     function __construct()
+    {
+         parent :: __construct();
+         $this -> load -> helper('html');
+		 $this->load->helper('url');
+		 //$this->load->library('javascript');
+		 //$this->output->cache(5);
+         }
+    
+     function _makeHeader(&$data)
+    {
+         echo doctype('html5');
+        $data['keywords'] = 'This, is, keywords';
+        $data['description'] = 'This is description';
+        $data['robots'] = 'This is robots part';
+		$baser = base_url();
+		$data['javascript'] = array();
+		$data['css'] = array($baser.'resource/common.css',$baser.'resource/missing/missing.css',
+							$baser.'resource/footer/footer.css',$baser.'resource/main/mbExtruder.css');
+    }
+    
+     function index()
+    {
+         $data = array();
+         $this -> _makeHeader($data);
+         $this -> load -> view('header', $data);
+         $this -> load -> view('missing');
+		 $this -> load -> view('footer');
+    }
+	
+    }
+?>
---
+++ application/views/missing.php
@@ -0,0 +1,7 @@
+<div id="p404-title">
+<h1>404 - 页面未找到</h1>
+</div>
+<div id="p404-imgframe">
+<img src="<?=base_url()?>resource/missing/404.png"/>
+<h3 style="padding:5px;">出现本页面的原因是您可能输入了一个错误的地址。 如果您确信这个地址无误，请及时与本站联系报错。</h3>
+</div>
