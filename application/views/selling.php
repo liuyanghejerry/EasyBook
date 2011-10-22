20244a27408c2461ce2fa272855b834e82623a8e
liuyanghejerry <liuyanghejerry@126.com>
2011-10-22 15:38:23 +0800

    add selling page.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

---
+++ application/views/selling.php
@@ -0,0 +1,163 @@
+<div class="frame">
+<div class="white-box" id="selling-nav">
+<h2>其它专业</h2>
+<ul>
+<li>+ <a href='#'>123</a></li>
+<li>+ <a href='#'>123</a></li>
+<li>+ <a href='#'>123</a></li>
+<li>+ <a href='#'>123</a></li>
+<li>+ <a href='#'>123</a></li>
+<li>+ <a href='#'>123</a></li>
+</ul>
+</div>
+
+<div class="white-box" id="selling-box">
+<h2>最新售出</h2>
+<div class="container">
+            <div class="wrapper">
+                <div id="st-accordion" class="st-accordion">
+                    <ul>
+                        <li>
+                            <a href="#">经济法<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
+                                <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
+                                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Web Design<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+                                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Illustrations<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>O my friend - but it is too much for my strength - I sink under the weight of the splendour of these visions!</p>
+                                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                                <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Miscancellous<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?"</p>
+                                <p>He thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">References<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
+                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Internship<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
+                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Jobs<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>O my friend - but it is too much for my strength - I sink under the weight of the splendour of these visions!</p>
+                                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                                <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Events<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
+                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Newsletter<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
+                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Contact<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
+                                <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.</p>
+                            </div>
+                        </li>
+						<li>
+                            <a href="#">Events<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
+                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Newsletter<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
+                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Contact<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
+                                <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.</p>
+                            </div>
+                        </li>
+						<li>
+                            <a href="#">Events<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
+                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Newsletter<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
+                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">Contact<span class="st-arrow">Open or Close</span></a>
+                            <div class="st-content">
+                                <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
+                                <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+			<div class="pager">
+			<span><a href='#'><<上一页</a> | <a href='#'>下一页>></a></span>
+			</div>
+        </div>
+        <script type="text/javascript">
+            $(function() {
+			
+				$('#st-accordion').accordion({
+					oneOpenedItem	: true
+				});
+				
+            });
+        </script>
+</div>
+<div class="white-box" id="selling-nav">
+<h2>其它学院</h2>
+<ul>
+<li>+ <a href='#'>123</a></li>
+<li>+ <a href='#'>123</a></li>
+<li>+ <a href='#'>123</a></li>
+<li>+ <a href='#'>123</a></li>
+<li>+ <a href='#'>123</a></li>
+<li>+ <a href='#'>123</a></li>
+</ul>
+</div>
+</div>
