9ca3352ebe7eeffff18603dce5e9b431577a7e83
liuyanghejerry <liuyanghejerry@126.com>
2011-10-23 13:54:29 +0800

    replenish the selling page.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- application/controllers/database_init.php
+++ application/controllers/database_init.php
@@ -73,7 +73,7 @@ class Database_init extends CI_Controller{
                                           ),
                         'captcha_time' => array(
                                                  'type' => 'INT',
-                                                 'constraint' => '10',
+                                                 'constraint' => 10,
 												 'unsigned' => TRUE
                                           ),
                         'ip_address' => array(
@@ -121,6 +121,137 @@ class Database_init extends CI_Controller{
 		 $this->dbforge->add_field($fields);
 		 $this->dbforge->add_key('session_id', TRUE);
 		 $this->dbforge->create_table('bk_sessions',TRUE);
+		 
+		 //collage category
+		 $fields = array(
+                        'collage_id' => array(
+                                                 'type' => 'INT',
+                                                 'constraint' => 5, 
+                                                 'unsigned' => TRUE,
+                                                 'auto_increment' => TRUE
+                                          ),
+                        'collage_name' => array(
+                                                 'type' => 'VARCHAR',
+                                                 'constraint' => '30'
+                                          ),
+         );
+		 $this->dbforge->add_field($fields);
+		 $this->dbforge->add_key('collage_id', TRUE);
+		 $this->dbforge->add_key('collage_name');
+		 $this->dbforge->create_table('bk_collages',TRUE);
+		 
+		 //subject category
+		 $fields = array(
+                        'subject_id' => array(
+                                                 'type' => 'INT',
+                                                 'constraint' => 5, 
+                                                 'unsigned' => TRUE,
+                                                 'auto_increment' => TRUE
+                                          ),
+                        'subject_name' => array(
+                                                 'type' => 'VARCHAR',
+                                                 'constraint' => '30'
+                                          ),
+						'collage_id' => array(
+                                                 'type' => 'INT',
+                                                 'constraint' => 5,
+												 'unsigned' => TRUE,
+												 'default' => 0
+                                          ),
+         );
+		 $this->dbforge->add_field($fields);
+		 $this->dbforge->add_key('subject_id', TRUE);
+		 $this->dbforge->add_key('subject_name');
+		 $this->dbforge->create_table('bk_subjects',TRUE);
+		 
+		 //books
+		 $fields = array(
+                        'selling_id' => array(
+                                                 'type' => 'INT',
+                                                 'constraint' => 9, 
+                                                 'unsigned' => TRUE,
+                                                 'auto_increment' => TRUE
+                                          ),
+                        'book_name' => array(
+                                                 'type' => 'VARCHAR',
+                                                 'constraint' => '30'
+                                          ),
+						'book_publisher' => array(
+                                                 'type' => 'VARCHAR',
+                                                 'constraint' => '30'
+                                          ),
+						'book_author' => array(
+                                                 'type' => 'VARCHAR',
+                                                 'constraint' => '30'
+                                          ),
+						'book_isbn' => array(
+                                                 'type' => 'BIGINT',
+                                                 'constraint' => 20,
+												 'unsigned' => TRUE
+                                          ),
+						'book_collage' => array(
+                                                 'type' => 'INT',
+                                                 'constraint' => 5,
+												 'unsigned' => TRUE
+                                          ),
+						'book_subject' => array(
+                                                 'type' => 'INT',
+                                                 'constraint' => 5,
+												 'unsigned' => TRUE
+                                          ),
+						'book_ownerid' => array(
+                                                 'type' => 'INT',
+                                                 'constraint' => 5,
+												 'unsigned' => TRUE
+                                          ),
+						'book_status' => array(
+                                                 'type' => 'INT',
+                                                 'constraint' => 5
+                                          ),
+						'book_boxart' => array(
+                                                 'type' => 'VARCHAR',
+                                                 'constraint' => 20
+                                          ),
+						'book_contact' => array(
+                                                 'type' => 'VARCHAR',
+                                                 'constraint' => 20
+                                          ),
+						'book_boxart' => array(
+                                                 'type' => 'VARCHAR',
+                                                 'constraint' => 20
+                                          ),
+						'book_note' => array(
+                                                 'type' => 'TEXT'
+                                          ),
+						'book_oprice' => array(
+                                                 'type' => 'VARCHAR',
+												 'constraint' => 10
+                                          ),
+						'book_nprice' => array(
+                                                 'type' => 'VARCHAR',
+												 'constraint' => 10
+                                          ),
+						'book_suggest' => array(
+                                                 'type' => 'INT',
+												 'constraint' => 5
+                                          ),
+						'selling_start' => array(
+                                                 'type' => 'DATE'
+                                          ),
+						'selling_end' => array(
+                                                 'type' => 'DATE'
+                                          ),
+         );
+		 $this->dbforge->add_field($fields);
+		 $this->dbforge->add_key('selling_id', TRUE);
+		 $this->dbforge->add_key('book_name');
+		 $this->dbforge->add_key('book_collage');
+		 $this->dbforge->add_key('book_subject');
+		 $this->dbforge->add_key('book_isbn');
+		 $this->dbforge->add_key('book_ownerid');
+		 $this->dbforge->add_key('book_status');
+		 $this->dbforge->add_key('selling_start');
+		 $this->dbforge->create_table('bk_selling',TRUE);
 	}
 	
     }
--- application/views/selling.php
+++ application/views/selling.php
@@ -17,126 +17,41 @@
             <div class="wrapper">
                 <div id="st-accordion" class="st-accordion">
                     <ul>
+                        <?php foreach($selling as $item):?>
                         <li>
-                            <a href="#">经济法<span class="st-arrow">Open or Close</span></a>
+                            <a href="#"><?=$item['book_name']?><span class="st-arrow">显示/隐藏</span></a>
                             <div class="st-content">
-                                <p>She packed her seven versalia, put her initial into the belt and made herself on the way.</p>
-                                <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
-                                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
+							<div class="boxart-slot">
+							<!--boxart of book -->
+							<img src="<?=base_url()?><?=$item['book_boxart']?>"/>
+							</div>
+							<div>
+								<div class="info-slot">
+								<h2>书籍信息</h2>
+                                <div class="slot"><span class="left-slot">书名：<?=$item['book_name']?></span><span class="right-slot">ISBN：<?=$item['book_isbn']?></span></div>
+								<div class="slot"><span class="left-slot">出版社：<?=$item['book_publisher']?></span><span class="right-slot">作者：<?=$item['book_author']?></span></div>
+								<div class="slot"><span class="left-slot">适用学院：<?=$item['book_collage']?></span><span class="right-slot">适用专业：<?=$item['book_subject']?></span></div>
+								<div class="slot"><span class="left-slot">原价：<?=$item['book_oprice']?></span><span class="right-slot">现价：<?=$item['book_nprice']?></span></div>
+								</div>
+								<div class="info-slot">
+								<h2>发布信息</h2>
+								<div class="slot"><span class="left-slot">发布时间：<?=$item['selling_start']?></span><span class="right-slot">过期时间：<?=$item['selling_end']?></span></div>
+								<div class="slot"><span class="left-slot">联系人：<?=$item['book_owner']?></span><span class="right-slot">联系方式：<?=$item['book_contact']?></span></div>
+								<div class="slot"><span class="left-slot">推荐次数：<?=$item['book_suggest']?></span></div>
+								<div class="slot"><span class="left-slot">备注：<?=$item['book_note']?></span></div>
+								</div>
+							</div>
                             </div>
                         </li>
-                        <li>
-                            <a href="#">Web Design<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#">Illustrations<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>O my friend - but it is too much for my strength - I sink under the weight of the splendour of these visions!</p>
-                                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
-                                <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#">Miscancellous<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked. "What's happened to me?"</p>
-                                <p>He thought. It wasn't a dream. His room, a proper human room although a little too small, lay peacefully between its four familiar walls.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#">References<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
-                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#">Internship<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
-                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#">Jobs<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>O my friend - but it is too much for my strength - I sink under the weight of the splendour of these visions!</p>
-                                <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
-                                <p>I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#">Events<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
-                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#">Newsletter<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
-                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#">Contact<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
-                                <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.</p>
-                            </div>
-                        </li>
-						<li>
-                            <a href="#">Events<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
-                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#">Newsletter<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
-                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#">Contact<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
-                                <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.</p>
-                            </div>
-                        </li>
-						<li>
-                            <a href="#">Events<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
-                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#">Newsletter<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer. Gregor then turned to look out the window at the dull weather.</p>
-                                <p>Drops of rain could be heard hitting the pane, which made him feel quite sad.</p>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#">Contact<span class="st-arrow">Open or Close</span></a>
-                            <div class="st-content">
-                                <p>No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
-                                <p>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.</p>
-                            </div>
-                        </li>
+						<?php endforeach;?>	
                     </ul>
                 </div>
             </div>
 			<div class="pager">
+			<!--
 			<span><a href='#'><<上一页</a> | <a href='#'>下一页>></a></span>
+			-->
+			<?=$pages?>
 			</div>
         </div>
         <script type="text/javascript">
