614c65a592c961d010f9804b55aa78d39d21a7b6
liuyanghejerry <liuyanghejerry@126.com>
2011-10-30 00:04:32 +0800

    new model of collage/subject data and dropdown for them.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

---
+++ application/models/collageModel.php
@@ -0,0 +1,37 @@
+<?php
+class CollageModel extends CI_Model {
+
+    var $data;
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+	
+	function allCollage(&$data)
+	{
+		$sql = "SELECT * FROM `bk_collages` WHERE `collage_id` != 1 ORDER BY `collage_id`";
+		$query = $this->db->query($sql);
+		$i = 0;
+		if ($query->num_rows() > 0)
+			{
+			   foreach ($query->result() as $row)
+			   {
+				  $data['collages'][$i]['subjects'] = $this->allSubject($row->collage_id);
+				  $data['collages'][$i]['name'] = $row->collage_name;
+				  $data['collages'][$i]['id'] = $row->collage_id;
+				  $i++;
+			   }
+			}
+	}
+	
+	function allSubject($id)
+	{
+		$sql = "SELECT * FROM `bk_subjects` WHERE `collage_id` = ? ORDER BY `subject_id`";
+		$query = $this->db->query($sql,array($id));
+		return $query->result_array();
+	
+	}
+	
+}
+?>
