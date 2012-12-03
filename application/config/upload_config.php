ded207af1a2e1436b631297cb8f88c339eabce24
liuyanghejerry <liuyanghejerry@126.com>
2012-12-03 14:42:27 +0800

    new: move upload settings to a single config file.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

---
+++ application/config/upload_config.php
@@ -0,0 +1,21 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Upload settings of selling book boxart
+|--------------------------------------------------------------------------
+| 
+| max resolution, etc
+*/
+
+$config['upload_path'] = './resource/uploads/';
+$config['allowed_types'] = 'gif|jpg|png|jpeg';
+$config['max_size'] = '2048';
+$config['max_width']  = '720';
+$config['max_height']  = '1080';
+$config['encrypt_name']  = TRUE;
+
+
+
+/* End of file upload_config.php */
+/* Location: ./application/config/upload_config.php */
